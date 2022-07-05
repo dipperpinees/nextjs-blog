3911eb1a8658c607b826f12e288c1fb4961b5b53
hiepnk223 <hiepnguyenno01@gmail.com>
2022-07-05 22:09:56 +0700

    Add categories object

--- lib/category.ts
+++ lib/category.ts
@@ -1,34 +1,42 @@
 export const categoriesList = [
     {
-        id: 8,
-        title: 'Quan điểm - Tranh luận',
-    },
-    {
         id: 1,
         title: 'Khoa học - Công nghệ',
-    },
-    {
-        id: 3,
-        title: 'Game',
-    },
-    {
-        id: 4,
-        title: 'Phim',
-    },
-    {
-        id: 5,
-        title: 'Sách',
+        emoji: ' 🦾'
     },
     {
         id: 2,
         title: 'Thể thao',
+        emoji: '⛹🏼‍♂️'
     },
     {
-        id: 7,
-        title: 'Trò chuyện - Tâm sự',
+        id: 3,
+        title: 'Game',
+        emoji: '💻'
+    },
+    {
+        id: 4,
+        title: 'Phim',
+        emoji: '🎥'
+    },
+    {
+        id: 5,
+        title: 'Sách',
+        emoji: '📖'
     },
     {
         id: 6,
         title: 'Âm nhạc',
+        emoji: '🎸'
     },
+    {
+        id: 7,
+        title: 'Trò chuyện - Tâm sự',
+        emoji: '🫂'
+    },
+    {
+        id: 8,
+        title: 'Quan điểm - Tranh luận',
+        emoji: '🤷‍♂️'
+    }, 
 ];
